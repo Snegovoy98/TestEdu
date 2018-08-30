edadd6ccf7a0a30354de418a9bd8f0bd603187cf
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-08-31 00:27:48 +0300

    Import RegistrationType class and  Request class

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,11 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\RegistrationType;
 class RegistrationController extends AbstractController
 {
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('registration/registration.html.twig');
+         $form = $this->createForm(RegistrationType::class);
+
+         $form->handleRequest($request);
+         if ($form->isSubmitted() && $form->isValid()) {
+             $data = $form->getData();
+         }
+        return $this->render('registration/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
