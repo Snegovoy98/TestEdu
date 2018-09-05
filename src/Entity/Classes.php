38fc3e25085824f35ad53fa4d6dff1ef3bd76a9a
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-09-05 22:39:08 +0300

    Add Classes Entity

---
+++ src/Entity/Classes.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ClassesRepository")
+ */
+class Classes
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $class_number;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\School")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $school;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClassNumber(): ?int
+    {
+        return $this->class_number;
+    }
+
+    public function setClassNumber(int $class_number): self
+    {
+        $this->class_number = $class_number;
+
+        return $this;
+    }
+
+    public function getSchool(): ?School
+    {
+        return $this->school;
+    }
+
+    public function setSchool(?School $school): self
+    {
+        $this->school = $school;
+
+        return $this;
+    }
+}
