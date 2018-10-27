7173ead3516cda5cc8778c234e522ec141bcb285
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-10-27 20:38:35 +0300

    Remove surname, name, fathername, regions, cities, schools fields and add RepeatedType

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -7,12 +7,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\{TextType,
                                                 PasswordType,
+                                                RepeatedType,
                                                 BirthdayType,
                                                 EmailType,
                                                 ChoiceType,
                                                 SubmitType
                                                     };
-use App\Entity\{Regions, Cities, Schools, Classes};
 class RegistrationType extends AbstractType
 {
     /** This Type use for rendering registration form
@@ -22,27 +22,7 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $option)
     {
         $builder
-            ->add('surname', TextType::class, [
-             'attr' => ['class'       => 'form-control',
-                        'name'        => 'surname',
-                        'id'          => 'surname',
-                        'placeholder' =>   'Введите фамилию' ],
-               'label' => 'Фамилия'
-            ])
-            ->add('name', TextType::class, [
-              'attr' => ['class'       => 'form-control',
-                         'name'        => 'name',
-                         'id'          =>  'name',
-                         'placeholder' =>   'Введите имя' ],
-                'label' => 'Имя'
-            ])
-            ->add('fathername', TextType::class, [
-               'attr' => ['class'       => 'form-control',
-                          'name'        => 'fathername',
-                          'id'          => 'fathername',
-                          'placeholder' =>   'Введите отчество'],
-                'label' => 'Отчество'
-            ])
+
             ->add('email', EmailType::class, [
                 'attr' => ['class'        => 'form-control',
                            'name'         => 'email',
@@ -50,66 +30,29 @@ class RegistrationType extends AbstractType
                            'placeholder'  => 'Введите email'],
                 'label' => 'Email'
             ])
-            ->add('password', PasswordType::class, [
+            ->add('username', TextType::class, [
+                'attr' => ['class'       => 'form-control',
+                    'name'        => 'username',
+                    'id'          => 'username',
+                    'placeholder' =>   'Введите имя пользователя'],
+                'label' => 'Имя пользователя'
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'attr' => ['class'       => 'form-control',
                            'name'        => 'password',
                            'id'          => 'password',
                            'placeholder' =>   'Введите пароль'],
-                'label' => 'Пароль'
-
+                'first_options'  => ['label' => 'Пароль'],
+                'second_options' => ['label' => 'Повторите пароль']
             ])
-            ->add('password_conf', PasswordType::class, [
-                'attr' => ['class'        => 'form-control',
-                           'name'         => 'password_conf',
-                           'id'           => 'password_conf',
-                           'placeholder'  => 'Введите повторно пароль'],
-                'label' => 'Повторите пароль'
-            ])
-            ->add('born', BirthdayType::class, [
+            ->add('date_born', BirthdayType::class, [
                 'attr' => ['class'  => 'custom-select d-block w-100',
                            'name'   => 'born_date',
                             'id'    => 'born_date'],
                 'widget' => 'single_text',
                 'format' => 'yyyy-MM-dd',
                 'label' => 'Дата рождение'
-            ])
-            ->add('regions', EntityType::class, [
-               'class' => Regions::class,
-                'attr' => ['class' => 'custom-select d-block w-100',
-                    'name'  => 'region',
-                    'id'   => 'region',
-                    'placeholder' => 'Choose an option'
-                ],
-                'choice_label' => 'name',
-
-                'label' => 'Область'
-            ])
-            ->add('cities', EntityType::class, [
-                'attr' => ['class' => 'custom-select d-block w-100',
-                           'name'  => 'city',
-                            'id'   => 'city',
-                            'placeholder' => 'Choose an option'],
-                'class' => Cities::class,
-                'choice_label' => 'name',
-                'label' => 'Город'
-            ])
-            ->add('school', EntityType::class, [
-                'attr' => ['class' => 'custom-select d-block w-100',
-                           'name'  => 'school',
-                           'id'   => 'school',
-                           'placeholder' => 'Choose an option'],
-                'class' => Schools::class,
-                'choice_label' => 'name',
-                'label' => 'Школа'
-            ])
-            ->add('class', EntityType::class, [
-                'attr' => ['class' => 'custom-select d-block w-100',
-                           'name'  => 'classes',
-                           'id'   => 'classes',
-                           'placeholder' => 'Choose an option'],
-                'class' => Classes::class,
-                'choice_label' => 'class_number',
-                'label' => 'Класс'
             ])
             ->add('gender', ChoiceType::class, [
                 'attr' => ['class' => 'custom-select d-block w-100',
