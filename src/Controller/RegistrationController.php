de647f4ad74ba74dd82abd748313b35242dfa24e
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-10-31 21:17:32 +0200

    Add registration algorithm

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\RegistrationType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 class RegistrationController extends AbstractController
 {
-    public function index(Request $request)
+    /** This Controller use for registration user in platform
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
-         $form = $this->createForm(RegistrationType::class);
+        $users = new Users();
+         $form = $this->createForm(RegistrationType::class, $users);
 
          $form->handleRequest($request);
          if ($form->isSubmitted() && $form->isValid()) {
-             $data = $form->getData();
+             $password = $passwordEncoder->encodePassword($users, $users->getPassword());
+             $users->setPassword($password);
+             $entityManager = $this->getDoctrine()->getManager();
+             $entityManager->persist($users);
+             $entityManager->flush();
+             return $this->redirectToRoute('login');
          }
         return $this->render('registration/registration.html.twig', [
             'form' => $form->createView()
