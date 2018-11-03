e2686a0c342dd740f3e92310eda45c087c86391b
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-11-03 22:57:06 +0200

    Remove Entity type

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\{TextType,
