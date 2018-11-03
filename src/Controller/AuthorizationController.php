84e901642dc9dd80cd3ed2bef59c536cd639cf92
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-11-04 00:09:12 +0200

    Refactor AuthorizationController: add object Response and PHPDoc

--- src/Controller/AuthorizationController.php
+++ src/Controller/AuthorizationController.php
@@ -3,18 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthorizationController extends AbstractController
 {
-    public function index(AuthenticationUtils $authenticationUtils)
+    /** This class  use for authorization user in application
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
+        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('authorization/authorization.html.twig', [
             'last_username' => $lastUsername,
-            'error'         => $error,
+            'error' => $error
         ]);
     }
 }
