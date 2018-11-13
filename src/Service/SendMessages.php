436cc7435a80f8ecc502fe8722cea7bd20612161
Snegovoy98 <snegovoy.aleksandr@gmail.com>
2018-11-13 22:45:16 +0200

    Add email our team and change algorithm for sending messages

--- src/Service/SendMessages.php
+++ src/Service/SendMessages.php
@@ -2,22 +2,28 @@
 
 namespace App\Service;
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class SendMessages
+class SendMessages implements ContainerAwareInterface
 {
-    private $receiver;
-    private $message;
-    private $firstName;
-    private $lastName;
+    use ContainerAwareTrait;
     private $mailer;
+    private $teamMail = 'TestEducationTeam@gmail.com';
 
     public function __construct(\Swift_Mailer $mailer)
     {
         $this->mailer = $mailer;
     }
 
-    public function index()
+    public function index(array $data): bool
     {
-        
+        $message = (new \Swift_Message("Добро пожаловать на сервис testEdu, рады приветствовать"))
+            ->setFrom($this->teamMail)
+            ->setTo($data['email'])
+            ->setBody('Спасибо вам огромное за то, что выбрали нас!');
+        ;
+        return (bool) $this->mailer->send($message);
     }
 }
